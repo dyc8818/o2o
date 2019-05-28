c93f665166bffcedfe572de75259d01890c26891
dy <387768965@qq.com>
2019-05-28 21:03:14 +0800

    init

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        Main main = new Main();
+        main.main(new String[]{"1"});
+    }
+}
