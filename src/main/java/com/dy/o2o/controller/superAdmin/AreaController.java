942109585328ea6af4bd35b4245c40ac562af0ee
dy <387768965@qq.com>
2019-05-30 18:15:53 +0800

    日志记录

--- src/main/java/com/dy/o2o/controller/superAdmin/AreaController.java
+++ src/main/java/com/dy/o2o/controller/superAdmin/AreaController.java
@@ -2,6 +2,8 @@ package com.dy.o2o.controller.superAdmin;
 
 import com.dy.o2o.entity.Area;
 import com.dy.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +18,15 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     AreaService areaService;
 
     @ResponseBody
     @RequestMapping(value = "/listarea", method = RequestMethod.GET)
     private Map<String, Object> listArea() {
+        logger.info("===start====");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMap = new HashMap<>();
         List<Area> list = new ArrayList<>();
         try {
@@ -31,6 +36,10 @@ public class AreaController {
         }catch (Exception e){
             e.printStackTrace();
         }
+        logger.error("test error!");
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime-startTime);
+        logger.info("====end====");
         return modelMap;
     }
 }
